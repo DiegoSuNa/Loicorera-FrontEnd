cca4a5e3f5ec0cef80053389339401f586cd02ca
Cailepaquemebaile <mateo2523299@gmail.com>
2021-11-28 17:49:04 -0500

    add server node

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/prueba-frontend'));
+app.use(express.static('./dist/PruebasAngular11'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/prueba-frontend/'}),
+    res.sendFile('index.html', {root: 'dist/PruebasAngular11/'}),
 );
 
 // Start the app by listening on the default Heroku port
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'https://licorera-b12-mintic.herokuapp.com/'
+  apiUrl: 'http://localhost:3000'
 };
 
 
